51830aad7683ec66cfb40a0b0d58d169e64d2b2b
falconrepbkp <carmilalily2@gmail.com>
2026-03-14 04:21:46 +0600

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     const country = context.request.cf?.country;
-    if (country === "BD") {
+    if (country === "IN") {
       return new Response("Access restricted in your region.", { status: 403 });
     }
     return context.next();
